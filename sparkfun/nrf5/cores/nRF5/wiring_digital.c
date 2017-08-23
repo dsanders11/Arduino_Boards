5cd7366ce2a577e524c40ae2060148fa007d397d
David Sanders <dsanders11@ucsbalum.com>
2017-08-23 15:49:36 -0600

    Add high drive output

--- sparkfun/nrf5/cores/nRF5/wiring_digital.c
+++ sparkfun/nrf5/cores/nRF5/wiring_digital.c
@@ -72,6 +72,15 @@ void pinMode( uint32_t ulPin, uint32_t ulMode )
                                | ((uint32_t)GPIO_PIN_CNF_SENSE_Disabled   << GPIO_PIN_CNF_SENSE_Pos);
     break ;
 
+    case OUTPUT_HIGH:
+      // Set pin to output mode
+      NRF_GPIO->PIN_CNF[ulPin] = ((uint32_t)GPIO_PIN_CNF_DIR_Output       << GPIO_PIN_CNF_DIR_Pos)
+                               | ((uint32_t)GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos)
+                               | ((uint32_t)GPIO_PIN_CNF_PULL_Disabled    << GPIO_PIN_CNF_PULL_Pos)
+                               | ((uint32_t)GPIO_PIN_CNF_DRIVE_H0H1       << GPIO_PIN_CNF_DRIVE_Pos)
+                               | ((uint32_t)GPIO_PIN_CNF_SENSE_Disabled   << GPIO_PIN_CNF_SENSE_Pos);
+    break ;
+
     default:
       // do nothing
     break ;
